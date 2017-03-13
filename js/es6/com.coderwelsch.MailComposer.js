1cb9a1d2ed61946794afc2c8cc5d8fb7c686e583
Joseph Ribbe <social@coderwelsch.com>
2017-03-13 11:54:40 +0100

    [MailComposer.js] Converted from jQery to Query

--- js/es6/com.coderwelsch.MailComposer.js
+++ js/es6/com.coderwelsch.MailComposer.js
@@ -1,21 +1,34 @@
 // imports
 import FormValidator from "./com.coderwelsch.FormValidator.js";
+import $ from "./com.coderwelsch.Query.js";
+import Utils from "./com.coderwelsch.Utils.js";
 
-class MailComposer {
+export default class MailComposer {
 	constructor ( settings ) {
 		this.settings = {
-			selectors: {},
+			selectors: {
+				container: "form",
+				field: ".field",
+				input: "input, textarea",
+				sendButton: "#send-mail",
+				sendButtonClickable: ".active:not(.waiting, .disabled)",
+				isWaiting: ".waiting"
+			},
 			phpSendMailPath: "./files/php/modules/send-mail.php",
 			tinyMCESettings: {
 				selector: "#mail-composer",
 				min_height: 250,
 				menubar: false,
 				content_css: "./files/css/mail.css"
+			},
+			callbacks: {
+				emailSend: () => {},
+				emailError: () => {}
 			}
 		};
 
 		// extend settings
-		window.$.extend( true, this.settings, settings );
+		this.settings = Utils.extend( true, this.settings, settings );
 
 		if ( window.tinymce === undefined ) {
 			throw Error( "No tinymce instance set in settings!" );
@@ -85,22 +98,51 @@ class MailComposer {
 		return this.formValidatorInstance.checkValidForm() && this.formValidatorInstance.validateInputByString( this.message );
 	}
 
+	resetInput () {
+		let $container = new $( this.selectors.container );
+
+		if ( window.tinyMCE ) {
+			window.tinyMCE.activeEditor.setContent( "" );
+		}
+
+		$container.find( this.selectors.input ).each( ( elem ) => {
+			elem.value = "";
+		} );
+	}
+
 	sendMail ( formData, callback ) {
 		formData[ "user-message" ] = this.message;
 
-		window.$.post( {
+		$.ajax( {
+			type: "POST",
 			url: this.settings.phpSendMailPath,
 			data: formData,
-			dataType: "json",
-			success: function ( data ) {
-				if ( data.success ) {
-					callback( true );
-				} else {
+
+			callbacks: {
+				done: ( data ) => {
+					try {
+						data = JSON.parse( data.responseText );
+
+						if ( data.success ) {
+							callback( true );
+
+							if ( typeof this.settings.callbacks.emailSend === "function" ) {
+								this.settings.callbacks.emailSend();
+							}
+						} else {
+							callback( false );
+
+							if ( typeof this.settings.callbacks.emailError === "function" ) {
+								this.settings.callbacks.emailError();
+							}
+						}
+					} catch ( error ) {
+						callback( false );
+					}
+				},
+				fail: () => {
 					callback( false );
 				}
-			},
-			error: function () {
-				callback( false );
 			}
 		} );
 
@@ -108,5 +150,3 @@ class MailComposer {
 		return true;
 	}
 }
-
-module.exports = MailComposer;
